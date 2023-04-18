5a51e8e826b9ff8fa75e45f2dd5bd1cf1ea79481
조승현 <sansusjc3@naver.com>
2023-04-18 10:18:40 +0900

    [BE] feat : initialize spring project

---
+++ mention/src/main/java/com/ssafy/c105/mention/MentionApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.c105.mention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentionApplication.class, args);
+	}
+
+}
---
+++ mention/src/test/java/com/ssafy/c105/mention/MentionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.c105.mention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
