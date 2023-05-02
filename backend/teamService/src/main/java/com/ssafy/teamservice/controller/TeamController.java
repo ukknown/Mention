592cd558fefe9e1cbbde55767bcb191631d04f35
임세은 <limse0204@naver.com>
2023-05-02 15:45:20 +0900

    [back] feat : TeamController 생성

---
+++ backend/teamService/src/main/java/com/ssafy/teamservice/controller/TeamController.java
@@ -0,0 +1,14 @@
+package com.ssafy.teamservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/team-service")
+public class TeamController {
+    @GetMapping("/health-check")
+    public String checkConnection(){
+        return "MemberService Check Completed!";
+    }
+}
