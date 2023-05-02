029cd5809c8e487fcc87eb4ae0de679743f3783a
임세은 <limse0204@naver.com>
2023-05-02 15:44:28 +0900

    [back] feat : env 변수 삭제

--- backend/memberService/src/main/java/com/ssafy/memberservice/controller/MemberController.java
+++ backend/memberService/src/main/java/com/ssafy/memberservice/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.ssafy.memberservice.controller;
 
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/member-service")
 public class MemberController {
-    private final Environment env;
-
-    public MemberController(Environment env) {
-        this.env = env;
-    }
-
     @GetMapping("/health-check")
     public String checkConnection(){
-        return "MemberService Check Completed!" + env.getProperty("greeting.message");
+        return "MemberService Check Completed!");
     }
 
 }
