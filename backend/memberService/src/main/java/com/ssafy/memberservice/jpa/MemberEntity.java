db8e082f2b73d392cdf2e74b35d511a3cfddaeb4
임세은 <limse0204@naver.com>
2023-05-02 13:50:41 +0900

    [back] feat : 엔티티 정보 추가

--- backend/memberService/src/main/java/com/ssafy/memberservice/jpa/MemberEntity.java
+++ backend/memberService/src/main/java/com/ssafy/memberservice/jpa/MemberEntity.java
@@ -1,5 +1,18 @@
 package com.ssafy.memberservice.jpa;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
