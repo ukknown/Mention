a281a88dd410d3c0d06cc0cd8ffbd84ad08c4ede
LIM SEEUN <limse0204@naver.com>
2023-05-05 08:15:21 +0900

    [back] feat : team 엔티티 컬럼 추가

--- backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamEntity.java
+++ backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamEntity.java
@@ -1,18 +1,22 @@
 package com.ssafy.teamservice.jpa;
 
 import javax.persistence.*;
-
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "Team")
+@Table(name = "team")
 public class TeamEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
+    @Column(length = 100, nullable = false)
+    private String name;
+    @Column(length = 1000, nullable = false)
+    private String image;
+    @Column(columnDefinition = "integer default 0")
+    private Integer capacity;
+    @Column(length = 100, nullable = false)
+    private String code;
 }
