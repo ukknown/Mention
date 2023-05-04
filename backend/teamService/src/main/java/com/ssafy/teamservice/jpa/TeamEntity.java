5f2bfdcc5fd3b8177da09db6fe7ed429d3754cc5
LIM SEEUN <limse0204@naver.com>
2023-05-04 11:32:17 +0900

    [back] feat : Team 테이블명 추가

--- backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamEntity.java
+++ backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamEntity.java
@@ -1,18 +1,18 @@
 package com.ssafy.teamservice.jpa;
 
-import javax.persistence.Access;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "Team")
 public class TeamEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
 }
