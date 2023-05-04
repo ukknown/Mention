d72eba3ce6769170e837fbaefde325a9314be774
조승현 <sansusjc3@naver.com>
2023-05-04 16:25:59 +0900

    [back] feat: topic 프로젝트 생성

---
+++ backend/topicService/src/test/java/com/ssafy/topicservice/TopicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.topicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
