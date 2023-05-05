05086aaef7932a65da750cf28eda5068b6c26c21
LIM SEEUN <limse0204@naver.com>
2023-05-05 19:42:52 +0900

    [back] feat : 팀 고유 아이디로 존재 여부 확인 기능 추가

--- backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamRepository.java
+++ backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeamRepository extends CrudRepository<TeamEntity, Long> {
-
+    boolean existsById(Long teamId);
 }
