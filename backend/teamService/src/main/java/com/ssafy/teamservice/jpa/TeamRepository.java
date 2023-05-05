8811406344582c33f84b9fa38f655cc31b5cae35
LIM SEEUN <limse0204@naver.com>
2023-05-05 11:39:47 +0900

    [back] feat : @Repository 추가

--- backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamRepository.java
+++ backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamRepository.java
@@ -1,7 +1,9 @@
 package com.ssafy.teamservice.jpa;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeamRepository extends CrudRepository<TeamEntity, Long> {
 
 }
