4dc0514bbbbbf8f19ad98f5da9f07b1ff252cb0d
LIM SEEUN <limse0204@naver.com>
2023-05-05 19:43:55 +0900

    [back] feat : Swagger 환경 설정 파일 추가

---
+++ backend/teamService/src/main/java/com/ssafy/teamservice/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.ssafy.teamservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        Info info = new Info()
+                .title("멘션 API Documentation")
+                .version("v1.0.0")
+                .description("모이다 API에 대한 설명 문서입니다!");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(Type.HTTP)
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .in(In.HEADER).name("Authorization");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .info(info);
+    }
+}
