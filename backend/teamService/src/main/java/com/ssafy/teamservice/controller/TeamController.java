78b7e73ec2c2c997b95fa082bc643b77d9e0d8e1
LIM SEEUN <limse0204@naver.com>
2023-05-05 12:20:45 +0900

    [back] feat : 팀 생성 api 추가

--- backend/teamService/src/main/java/com/ssafy/teamservice/controller/TeamController.java
+++ backend/teamService/src/main/java/com/ssafy/teamservice/controller/TeamController.java
@@ -1,14 +1,42 @@
 package com.ssafy.teamservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.teamservice.service.TeamServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/team-service")
 public class TeamController {
+    private final TeamServiceImpl teamServiceImpl;
+
+    public TeamController(TeamServiceImpl teamServiceImpl) {
+        this.teamServiceImpl = teamServiceImpl;
+    }
+
+    /**
+     * MSA 연결 확인
+     * @return
+     */
     @GetMapping("/health-check")
     public String checkConnection(){
         return "Team MicroService Check Completed!";
+    }
+
+    /**
+     * 그룹(팀) 생성
+     * @param name
+     * @param file
+     * @return
+     */
+    @PostMapping(path = "/teams", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity createTeam(
+            @RequestPart(value = "name") String name,
+            @RequestPart(value = "file", required = false) MultipartFile file
+    ){
+        teamServiceImpl.createTeam(name, file);
+        return new ResponseEntity(null, HttpStatus.OK);
     }
 }
