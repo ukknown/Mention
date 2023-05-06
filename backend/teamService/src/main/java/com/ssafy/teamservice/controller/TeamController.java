935384782598f7ca7b98236663fbf32f0ed9c000
LIM SEEUN <limse0204@naver.com>
2023-05-06 11:13:55 +0900

    [back] feat : 그룹 입장 추가

--- backend/teamService/src/main/java/com/ssafy/teamservice/controller/TeamController.java
+++ backend/teamService/src/main/java/com/ssafy/teamservice/controller/TeamController.java
@@ -1,15 +1,19 @@
 package com.ssafy.teamservice.controller;
 
+import com.ssafy.teamservice.jpa.TeamEntity;
+import com.ssafy.teamservice.service.TeamMemberServiceImpl;
 import com.ssafy.teamservice.service.TeamServiceImpl;
 import com.ssafy.teamservice.utils.RandomCodeGenerator;
 import com.ssafy.teamservice.utils.S3Uploader;
 import com.ssafy.teamservice.utils.error.ErrorCode;
 import com.ssafy.teamservice.utils.exception.CustomException;
 import com.ssafy.teamservice.vo.TeamDetailsResponseDto;
+import com.ssafy.teamservice.vo.TeamMemberVO;
 import com.ssafy.teamservice.vo.TeamVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.*;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,11 +24,13 @@ public class TeamController {
     private final TeamServiceImpl teamServiceImpl;
     private final S3Uploader s3Uploader;
     private final RandomCodeGenerator randomCodeGenerator;
+    private final TeamMemberServiceImpl teamMemberServiceImpl;
 
-    public TeamController(TeamServiceImpl teamServiceImpl, S3Uploader s3Uploader, RandomCodeGenerator randomCodeGenerator) {
+    public TeamController(TeamServiceImpl teamServiceImpl, S3Uploader s3Uploader, RandomCodeGenerator randomCodeGenerator, TeamMemberServiceImpl teamMemberServiceImpl) {
         this.teamServiceImpl = teamServiceImpl;
         this.s3Uploader = s3Uploader;
         this.randomCodeGenerator = randomCodeGenerator;
+        this.teamMemberServiceImpl = teamMemberServiceImpl;
     }
 
     /**
@@ -44,7 +50,9 @@ public class TeamController {
      * @return
      */
     @PostMapping(path = "/teams", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Transactional
     public ResponseEntity createTeam(
+            @RequestPart(value = "memberid") Long memberId,
             @RequestPart(value = "name") String name,
             @RequestPart(value = "file", required = false) MultipartFile file
     ){
@@ -53,13 +61,15 @@ public class TeamController {
 
         // String code = randomCodeGenerator.generate();
 
-        teamServiceImpl.createTeam(new TeamVO(name, url, 1, false));
+        TeamEntity teamEntity = teamServiceImpl.createTeam(new TeamVO(name, url));
+
+        teamMemberServiceImpl.joinTeamMember(new TeamMemberVO(teamEntity, memberId));
 
         return ResponseEntity.status(HttpStatus.OK).body("팀 생성 완료");
     }
 
     /**
-     * 그룹 상세정보 조회
+     * 그룹 상세정보 조회 + 투표 조회 추가
      * @param teamId
      * @return
      */
@@ -73,8 +83,35 @@ public class TeamController {
         TeamDetailsResponseDto result = teamServiceImpl.getTeamDetails(teamId);
 
         // 해당 방이 삭제된 경우 -> 404 반환
-        if(result.isDeleted()) throw new CustomException(ErrorCode.DATA_NOT_FOUND);
+        if(result.getIsDeleted() == 1) throw new CustomException(ErrorCode.DATA_NOT_FOUND);
 
         return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    /**
+     * 생성된 팀 입장
+     * @param teamId
+     * @param memberId
+     * @return
+     */
+    @PostMapping("/teams/{teamid}")
+    public ResponseEntity joinTeam(
+            @PathVariable("teamid") Long teamId,
+            @RequestBody Long memberId
+    ){
+        // teamId가 존재하는지 확인 -> 404
+        if(!teamServiceImpl.existsById(teamId)) throw new CustomException(ErrorCode.DATA_NOT_FOUND);
+
+        TeamEntity teamEntity = teamServiceImpl.findById(teamId);
+        TeamMemberVO teamMemberVO = new TeamMemberVO(teamEntity, memberId);
+
+        // 이미 입장한 그룹인지 확인
+        if(teamMemberServiceImpl.existsByMemberIdAndTeamEntity(teamMemberVO)){
+            throw new CustomException(ErrorCode.CONFLICT_TEAM_MEMBER);
+        }
+
+        teamMemberServiceImpl.joinTeamMember(teamMemberVO);
+
+        return ResponseEntity.status(HttpStatus.OK).body("팀 입장 완료 ~ 🔥");
     }
 }
