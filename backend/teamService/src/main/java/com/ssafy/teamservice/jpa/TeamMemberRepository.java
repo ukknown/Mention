95ae08f53f7d66be306149f86e6b4578d0823050
LIM SEEUN <limse0204@naver.com>
2023-05-06 12:34:08 +0900

    [back] feat : 그룹에서 회원 삭제 추가

--- backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamMemberRepository.java
+++ backend/teamService/src/main/java/com/ssafy/teamservice/jpa/TeamMemberRepository.java
@@ -3,7 +3,12 @@ package com.ssafy.teamservice.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeamMemberRepository extends JpaRepository<TeamMemberEntity, Long> {
     boolean existsByMemberIdAndTeamEntity(Long memberId, TeamEntity teamEntity);
+    TeamMemberEntity findByMemberIdAndTeamEntity(Long memberId, TeamEntity teamEntity);
+    List<TeamMemberEntity> findTeamMemberEntityByMemberId(Long memberId);
+    void deleteById(Long teamMemberId);
 }
