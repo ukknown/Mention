521ec4ffb92c23ac07db542301dc195133c45105
jongukchoi95 <jongukchoi95@gmail.com>
2023-05-07 22:07:46 +0900

    [back] feat : service에 infoDto(id, role) 보내는 코드 작성

--- backend/gatewayService/src/main/java/com/ssafy/gatewayservice/filter/AuthorizationHeaderFilter.java
+++ backend/gatewayService/src/main/java/com/ssafy/gatewayservice/filter/AuthorizationHeaderFilter.java
@@ -1,7 +1,11 @@
 package com.ssafy.gatewayservice.filter;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.gatewayservice.dto.InfoDto;
 import com.ssafy.gatewayservice.dto.MemberDto;
+import com.ssafy.gatewayservice.dto.Role;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -20,6 +24,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.Date;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -34,6 +39,8 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
     @Value("${jwt.secret}")
     private String secret_key;
+
+    private String accessToken;
 
     public AuthorizationHeaderFilter(Environment env, RedisTemplate redisTemplate) {
         super(Config.class);
@@ -53,11 +60,38 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
             String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
             String jwt = authorizationHeader.replace("Bearer","");
+            System.out.println("들어오는 accesstoken : "+jwt);
+            accessToken = jwt.trim();
+            jwt = jwt.trim();
+
+            System.out.println("test : " + redisTemplate.opsForValue().get(accessToken));
 
             if(!isJwtValid(jwt)){
                 return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
             }
+            //accessToken값이 변경되면 request header에 바뀐 accessToken값 추가
+//            if(!accessToken.equals(jwt)){
+//                exchange.getRequest().mutate().headers(httpHeaders -> httpHeaders.add("newAccess", accessToken)).build();
+//            }
             //여기서 redis접근 후 exchange에 담아서 보내줌
+            Map<String, Object> result = (Map<String, Object>) redisTemplate.opsForValue().get(accessToken);
+            Integer id = (Integer) result.get("id");
+            String role = (String) result.get("role");
+            System.out.println("header에 담을 정보 :" + id);
+
+            InfoDto infoDto = new InfoDto(id, role);
+            ObjectMapper mapper = new ObjectMapper();
+
+            try {
+                String memberStr = mapper.writeValueAsString(infoDto);
+
+                exchange.getRequest().mutate()
+                        .headers(httpHeaders -> httpHeaders.add("member", memberStr)).build();
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+
+
             return chain.filter(exchange);
         });
     }
@@ -72,49 +106,79 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
                     .parseClaimsJws(jwt).getBody()
                     .getSubject();
         }catch (Exception e){
-            returnValue = false;
-        }
-
-        if(subject == null || subject.isEmpty()){
-            returnValue = false;
-        }
-        //토큰 만료 확인
-        Claims claims = Jwts.parser().setSigningKey(env.getProperty("jwt.secret")).parseClaimsJws(jwt).getBody();
-
-        Date expriration = claims.getExpiration();
-        log.info(""+expriration);
-
-        if(expriration.before(new Date())){ //토큰 유효 시간이 현재 시간 이전이면 false
-            if(!isRefreshVaild(jwt)){ //refresh 유효시간 확인
+            if(!isRefreshVaild(jwt)) { //refresh 유효시간이 끝났으면
+                System.out.println("isJwtValid 만료");
                 returnValue = false;
+
+            }else{
+                System.out.println("isJwtValid 유효");
+                returnValue = true;
             }
-            returnValue = false;
         }
+
+//        if(subject == null || subject.isEmpty()){
+//            returnValue = false;
+//        }
+//        //토큰 만료 확인
+//        Claims claims = Jwts.parser().setSigningKey(env.getProperty("jwt.secret")).parseClaimsJws(jwt).getBody();
+//
+//        Date expriration = claims.getExpiration();
+//        log.info("",expriration);
+//
+//        if(expriration.before(new Date())){ //access 토큰 유효 시간이 현재 시간 이전이면
+//            if(!isRefreshVaild(jwt)) { //refresh 유효시간이 끝났으면
+//                returnValue = false;
+//            }
+//        }
 
         return returnValue;
     }
 
     private boolean isRefreshVaild(String jwt) {
-        MemberDto memberDto = (MemberDto) redisTemplate.opsForValue().get(jwt);
 
+        jwt = jwt.trim();
+//        String json = (String) redisTemplate.opsForValue().get(jwt);
+        Map<String, Object> result = (Map<String, Object>) redisTemplate.opsForValue().get(jwt);
+        Integer id = (Integer) result.get("id");
+        String email = (String) result.get("email");
+        String nickname = (String) result.get("nickname");
+        String role = (String) result.get("role");
+        String refreshToken = (String) result.get("refresh");
+
+        System.out.println("isRefreshVaild . redis에서 refresh토큰 : "+refreshToken);
+
+        MemberDto memberDto = new MemberDto(id,email, nickname, role, refreshToken);
+
+//        ObjectMapper mapper = new ObjectMapper();
+//        MemberDto memberDto = mapper.convertValue(json, MemberDto.class);
+//
+//        //MemberDto memberDto = redisTemplate.opsForValue().get(jwt);
+//
+//
         String refresh = memberDto.getRefresh();
+
+        System.out.println(refresh);
 
         //refresh 만료 시간 확인
         Claims claims = Jwts.parser().setSigningKey(env.getProperty("jwt.secret")).parseClaimsJws(refresh).getBody();
 
-        Date expriration = claims.getExpiration();
+        Date refreshExpriration = claims.getExpiration();
+        System.out.println("isRefreshVaild에서 refresh만료 시간 : "+refreshExpriration);
 
         //현재 시간보다 이전이면 false
-        if(expriration.before(new Date())){
+        if(refreshExpriration.before(new Date())){
+            System.out.println("isRefreshVaild = refresh 만료");
             return false;
         }
-
+        System.out.println("isRefreshVaild = refresh 유효");
         //access재발급
         Claims reclaims = Jwts.claims().setSubject(memberDto.getEmail());
         reclaims.put("email", memberDto.getEmail());
         reclaims.put("nickname", memberDto.getNickname());
 
-        String accessToken = createAccessToken(reclaims);
+        accessToken = createAccessToken(reclaims);
+
+        System.out.println("새로 발급 된 access token : " + accessToken);
 
         redisTemplate.delete(jwt); //기존 access token 정보 제거
 
