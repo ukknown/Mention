aa39a2c8743c7d790347ba828616666881a25310
조승현 <sansusjc3@naver.com>
2023-05-08 08:57:20 +0900

    [back] feat : MentionEntity 컬럼 추가

--- backend/mentionService/src/main/java/com/ssafy/mentionservice/jpa/MentionEntity.java
+++ backend/mentionService/src/main/java/com/ssafy/mentionservice/jpa/MentionEntity.java
@@ -6,13 +6,31 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedEntityGraph;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Builder
 public class MentionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Long vote_id;
+
+    private Long voter_id;
+
+    private Long picker_id;
+
+    private String hint;
+
+    private LocalDateTime regDate;
+
+    private int hintStatus;
 }
