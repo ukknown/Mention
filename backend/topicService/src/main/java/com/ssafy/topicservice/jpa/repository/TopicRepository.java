75f4683151e8aac5f3c2b13ca403ba60bedb6f12
조승현 <sansusjc3@naver.com>
2023-05-07 14:43:52 +0900

    [back] feat : pending List dto 작성 및 jpa 코드 작성

--- backend/topicService/src/main/java/com/ssafy/topicservice/jpa/repository/TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.topicservice.jpa.repository;
-
-import com.ssafy.topicservice.jpa.Entity.Topic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface TopicRepository extends JpaRepository<Topic, Long> {
-
-}
