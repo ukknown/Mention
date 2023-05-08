4191db79db7a85db9494dd28ee1d71491107e8f2
조승현 <sansusjc3@naver.com>
2023-05-08 22:41:40 +0900

    [back] feat : swagger 작성

--- backend/topicService/src/main/java/com/ssafy/topicservice/controller/TopicController.java
+++ backend/topicService/src/main/java/com/ssafy/topicservice/controller/TopicController.java
@@ -1,5 +1,6 @@
 package com.ssafy.topicservice.controller;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import com.ssafy.topicservice.elastic.TopicDocument;
 import com.ssafy.topicservice.service.TopicService;
 import com.ssafy.topicservice.vo.TopicResoponseDto;
@@ -13,65 +14,68 @@ import java.util.Map;
 @RestController
 @RequestMapping("/topic-service")
 @RequiredArgsConstructor
+@Tag(name = "토픽 관리")
 public class TopicController {
 
     private final TopicService topicService;
+    @Operation(summary = "MSA 연결 체크")
     @GetMapping("/health-check")
     public String checkConnection(){
         return "TopicService Check Completed!";
     }
 
-    // elastic 저장 토픽 정해지면 토픽이 업데이트 되면 이 부분도 업데이트
+
+    @Operation(summary = "엘라스틱 repository에 저장", description = "데이터가 쌓이면 추후 메서드로 변경 예정")
     @PostMapping("/save/elastic")
     public void saveElastic() {
         topicService.saveElastic();
     }
 
-    // elastic 삭제  , 저장과 마찬가지로 jpa 업데이트 될 때 해당
+    @Operation(summary = "엘라스틱 repository에서 삭제", description = "데이터가 쌓이면 추후 메서드로 변경 예정")
     @PostMapping("/delete/elastic")
     public void delete() {
         topicService.deleteElastic();
     }
 
-    // 토픽 검색
+    @Operation(summary = "토픽 검색", description = "초성단위로는 안됨")
     @GetMapping("/elastic-search")
     public ResponseEntity<List<TopicDocument>> searchByTitle(@RequestParam String title) {
         List<TopicDocument> documents = topicService.searchByTitle(title);
         return ResponseEntity.ok().body(documents);
     }
 
-    //감정 분석 요청
+    @Operation(summary = "네이버 감정 분석 요청", description = "새로운 토픽일 경우 응모하시겠습니까? 이후 검증")
     @PostMapping("/call/naver")
     public ResponseEntity<String> goToNaver(@RequestBody Map<String, String> topicCandidate) {
         return ResponseEntity.ok().body(topicService.goToNaver(topicCandidate.get("topicCandidate")));
     }
 
-    //유사도 검사 요청
+    @Operation(summary = "새로운 토픽인지 아닌지 검증", description = "새로운 토픽인지 아닌지 검증")
     @PostMapping("/check/similarity")
     public ResponseEntity<String> checkSimilarity(@RequestBody Map<String, String> inputTopic) {
         return ResponseEntity.ok().body(topicService.checkSimilarity(inputTopic.get("inputTopic")));
     }
 
-    //토픽 저장
+    @Operation(summary = "토픽 저장", description = "추후 데이터 쌓이면 메서드로 변환 예정")
     @PostMapping("/save/topic")
     public void saveTopic() {
         topicService.saveTopic();
     }
 
-    // 관리자가 토픽 응모 리스트 조회
+    @Operation(summary = "관리자가 응모리스트를 조회", description = "PENDING 상태인것만 반환")
     @GetMapping("/admin/pendingList")
     public ResponseEntity<List<TopicResoponseDto>> getPendingTopic() {
         return ResponseEntity.ok().body(topicService.getPendingTopic());
     }
 
-    //관리자가 승인
+    @Operation(summary = "관리자가 응모 토픽을 승인", description = "APPROVE 상태로 변환")
     @PostMapping("/admin/changeStatus/approve")
     public ResponseEntity<?> approveTopic(@RequestBody Map<String, Long> topicId) {
         topicService.approveTopic(topicId.get("topicId"));
         return ResponseEntity.ok().body("승인 완료");
     }
 
-    //관리자가 거절
+    @Operation(summary = "관리자가 응모 토픽을 거절", description = "REJECT 상태로 변환")
     @PostMapping("/admin/changeStatus/reject")
     public ResponseEntity<?> rejectTopic(@RequestBody Map<String, Long> topicId) {
         topicService.rejectTopic(topicId.get("topicId"));
