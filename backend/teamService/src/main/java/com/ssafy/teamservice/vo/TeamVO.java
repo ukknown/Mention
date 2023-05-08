8edfa32475dc39f56fb9fb8d7e8681fe95caa158
LIM SEEUN <limse0204@naver.com>
2023-05-08 14:51:59 +0900

    [back] feat : @Setter 추가

--- backend/teamService/src/main/java/com/ssafy/teamservice/vo/TeamVO.java
+++ backend/teamService/src/main/java/com/ssafy/teamservice/vo/TeamVO.java
@@ -2,8 +2,10 @@ package com.ssafy.teamservice.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class TeamVO {
     private Long teamId;
