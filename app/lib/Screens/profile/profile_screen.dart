3108d85fda86c760af0cf4667f0df5f73326572c
njh3321 <njh3321@naver.com>
2023-05-09 17:54:08 +0900

    [front] feat : profile_screen futurebuilder 적용

--- app/lib/Screens/profile/profile_screen.dart
+++ app/lib/Screens/profile/profile_screen.dart
@@ -39,90 +39,95 @@ class ProfilePage extends StatelessWidget {
           } else {
             final profileData = snapshot.data!;
             print(profileData);
-            return Center(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: screenHeight * 0.075,
-                  ),
-                  ProfileCard(
-                    profileImage: profileData['profile_image'],
-                    name: profileData['name'],
-                    coin: profileData['coin'],
-                    groupCount: profileData['group_count'],
-                    mentionCount: profileData['mention_count'],
-                    screenHeight: screenHeight,
-                    screenWidth: screenWidth,
-                  ),
-                  SizedBox(
-                    height: screenHeight * 0.025,
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/images/running.png',
-                              height: screenWidth * 0.1,
-                            ),
-                            SizedBox(
-                              width: screenWidth * 0.02,
-                            ),
-                            Text(
-                              "다음 Bang까지 00걸음!",
-                              style: TextStyle(fontSize: screenWidth * 0.055),
-                            ),
-                          ],
-                        ),
-                        PedometerText(
-                          screenHeight: screenHeight,
-                          screenWidth: screenWidth,
-                        )
-                      ],
+            return SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: screenHeight * 0.1,
                     ),
-                  ),
-                  SizedBox(
-                    height: screenHeight * 0.05,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: screenWidth * 0.05),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/images/podium.png',
-                              height: screenWidth * 0.1,
-                            ),
-                            SizedBox(
-                              width: screenWidth * 0.02,
-                            ),
-                            Text(
-                              "나의 멘션 랭킹",
-                              style: TextStyle(fontSize: screenWidth * 0.055),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: screenHeight * 0.02,
-                        ),
-                        for (var entry in profileData['most_mentioned_topic']
-                            .asMap()
-                            .entries)
-                          RankSlot(
-                            screenWidth: screenWidth,
-                            screenHeight: screenHeight,
-                            topic: entry.value,
-                            rank: entry.key + 1,
+                    ProfileCard(
+                      profileImage: profileData['profile_image'],
+                      name: profileData['name'],
+                      coin: profileData['coin'],
+                      groupCount: profileData['group_count'],
+                      mentionCount: profileData['mention_count'],
+                      screenHeight: screenHeight,
+                      screenWidth: screenWidth,
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.05,
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset(
+                                'assets/images/running.png',
+                                height: screenWidth * 0.1,
+                              ),
+                              SizedBox(
+                                width: screenWidth * 0.02,
+                              ),
+                              Text(
+                                "다음 Bang까지 00걸음!",
+                                style: TextStyle(fontSize: screenWidth * 0.055),
+                              ),
+                            ],
                           ),
-                      ],
+                          PedometerText(
+                            screenHeight: screenHeight,
+                            screenWidth: screenWidth,
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: screenHeight * 0.05,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: screenWidth * 0.05),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset(
+                                'assets/images/podium.png',
+                                height: screenWidth * 0.1,
+                              ),
+                              SizedBox(
+                                width: screenWidth * 0.02,
+                              ),
+                              Text(
+                                "나의 멘션 랭킹",
+                                style: TextStyle(fontSize: screenWidth * 0.055),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: screenHeight * 0.02,
+                          ),
+                          for (var entry in profileData['most_mentioned_topic']
+                              .asMap()
+                              .entries)
+                            RankSlot(
+                              screenWidth: screenWidth,
+                              screenHeight: screenHeight,
+                              topic: entry.value,
+                              rank: entry.key + 1,
+                            ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.025,
+                    ),
+                  ],
+                ),
               ),
             );
           }
