539639fd645c66bf38e658dfb83a8e4f1a6a2ac7
LIM SEEUN <limse0204@naver.com>
2023-05-09 12:09:56 +0900

    [back] feat : @Service 어노테이션 추가

--- backend/notificationService/src/main/java/com/ssafy/notificationservice/service/FCMServiceImpl.java
+++ backend/notificationService/src/main/java/com/ssafy/notificationservice/service/FCMServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ssafy.notificationservice.service;
 
 import com.ssafy.notificationservice.vo.dto.FCMNotificationRequestDto;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FCMServiceImpl implements FCMService{
 
     @Override
