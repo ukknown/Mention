e218e9ce86302a707280e01b817b74d1fc2cc130
njh3321 <njh3321@naver.com>
2023-05-10 17:53:05 +0900

    [front] feat : 알림페이지 데이터 구성 중

--- app/lib/screens/mainPage.dart
+++ app/lib/screens/mainPage.dart
@@ -1,10 +1,8 @@
 import 'package:app/widgets/bottom_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:app/screens/profile/profile_page.dart';
-import 'package:circle_nav_bar/circle_nav_bar.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:app/screens/Hint.dart';
-import 'package:app/screens/home_screen.dart';
 import 'package:app/screens/group_screen.dart';
 
 class MainPage extends StatefulWidget {
