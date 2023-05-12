d45c430e1672f864ee0063fdecc24ae214974185
jongukchoi95 <jongukchoi95@gmail.com>
2023-05-12 14:25:02 +0900

    [back] feat : team에 feignClient요청

--- backend/memberService/src/main/java/com/ssafy/memberservice/service/FeignClient/TeamServiceFeignClient.java
+++ backend/memberService/src/main/java/com/ssafy/memberservice/service/FeignClient/TeamServiceFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "team-service")
+@FeignClient(name = "team-service", url = "http://localhost:8082/team-service")
 public interface TeamServiceFeignClient {
 
     @GetMapping("/GroupCount/{memberid}")
