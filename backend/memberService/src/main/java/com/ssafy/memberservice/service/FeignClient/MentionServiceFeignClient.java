deb30181bc0794d07c9644beb4695cccb4c6acc2
jongukchoi95 <jongukchoi95@gmail.com>
2023-05-12 14:24:40 +0900

    [back] feat : mention에 feignClient요청

--- backend/memberService/src/main/java/com/ssafy/memberservice/service/FeignClient/MentionServiceFeignClient.java
+++ backend/memberService/src/main/java/com/ssafy/memberservice/service/FeignClient/MentionServiceFeignClient.java
@@ -2,9 +2,9 @@ package com.ssafy.memberservice.service.FeignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient("/mention-service")
+@FeignClient(name = "mention-service", url ="http://localhost:8083/mention-service")
 public interface MentionServiceFeignClient {
 
 
-    int getMention(Long memberid);
+
 }
