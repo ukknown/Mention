590ccfa0e1a444f45e01086bf5089c207474ce46
조승현 <sansusjc3@naver.com>
2023-05-13 17:34:51 +0900

    [back] feat : url제거

--- backend/topicService/src/main/java/com/ssafy/topicservice/Feignclient/MemberServiceFeignClient.java
+++ backend/topicService/src/main/java/com/ssafy/topicservice/Feignclient/MemberServiceFeignClient.java
@@ -3,14 +3,12 @@ package com.ssafy.topicservice.Feignclient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
 
 
-@FeignClient(name = "member-service", url = "http://localhost:8081/member-service")
+@FeignClient(name = "member-service")
 public interface MemberServiceFeignClient {
-    @GetMapping("/time-out")
-    public ResponseEntity addTimeout(@RequestParam Long memberId);
+    @GetMapping("/member-service/time-out")
+    ResponseEntity addTimeout(@RequestParam Long memberId);
 }
