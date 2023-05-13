be39ced2ceb604ac19b8e22a7eaa2e0ca9dd4a6f
jongukchoi95 <jongukchoi95@gmail.com>
2023-05-13 16:43:03 +0900

    [back] feat : 카멜케이스로 요청 변경

--- backend/memberService/src/main/java/com/ssafy/memberservice/service/FeignClient/MentionServiceFeignClient.java
+++ backend/memberService/src/main/java/com/ssafy/memberservice/service/FeignClient/MentionServiceFeignClient.java
@@ -1,10 +1,13 @@
 package com.ssafy.memberservice.service.FeignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "mention-service", url ="http://localhost:8083/mention-service")
 public interface MentionServiceFeignClient {
 
-
+    //나를 가장 많이 멘션한 토픽의 멘션 수
+    @GetMapping("/mention-count")
+        Integer getMentionCount(Long memberid);
 
 }
--- backend/memberService/src/main/java/com/ssafy/memberservice/service/FeignClient/TopicServiceFeignClient.java
+++ backend/memberService/src/main/java/com/ssafy/memberservice/service/FeignClient/TopicServiceFeignClient.java
@@ -1,8 +1,15 @@
 package com.ssafy.memberservice.service.FeignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "topic-service", url ="http://localhost:8085/topic-service")
 public interface TopicServiceFeignClient {
+
+    //나를 가장 많이 멘션한 토픽
+    @GetMapping("/topic-service/top-topic/{memberid}")
+    String getTopTopic(@PathVariable  Long memberid);
+
 
 }
