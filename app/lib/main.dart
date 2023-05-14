65553a2167b68a112a0b9dd00daaea1014f747e3
kimchangyoung <kyle.kcy@gmail.com>
2023-05-14 18:21:13 +0900

    [front] feat : 알람시도 실패

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:app/Screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await AndroidAlarmManager.initialize();
+void main() {
   runApp(const Mention());
 }
 
