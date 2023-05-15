3b198305177b6246a14fbaf0a95707d8ff19ff45
LIM SEEUN <limse0204@naver.com>
2023-05-15 23:46:35 +0900

    [back] feat : ErrorCode 추가

--- backend/notificationService/src/main/java/com/ssafy/notificationservice/utils/error/ErrorCode.java
+++ backend/notificationService/src/main/java/com/ssafy/notificationservice/utils/error/ErrorCode.java
@@ -1,5 +1,15 @@
 package com.ssafy.notificationservice.utils.error;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@Getter
+@AllArgsConstructor
 public enum ErrorCode {
-    
+    INVALID_TOKEN(UNAUTHORIZED, "일치하는 토큰이 존재하지 않습니다.");
+    private final HttpStatus httpStatus;
+    private final String message;
 }
