a5a622b0c0cef42fc392af4472040c9209b10f31
조승현 <sansusjc3@naver.com>
2023-05-16 01:18:09 +0900

    [back] feat : feign clinet 추가

--- backend/mentionService/src/main/java/com/ssafy/mentionservice/feignclient/MemberServiceFeignClient.java
+++ backend/mentionService/src/main/java/com/ssafy/mentionservice/feignclient/MemberServiceFeignClient.java
@@ -1,8 +1,10 @@
 package com.ssafy.mentionservice.feignclient;
 
+import com.ssafy.mentionservice.vo.MemberInfoDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -10,4 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface MemberServiceFeignClient {
     @GetMapping("/member-service/feign/time-out")
     ResponseEntity addTimeout(@RequestParam Long memberId);
+    @GetMapping("/member-service/feign/member-info/{memberid}")
+    MemberInfoDto getMemberInfo(@PathVariable Long memberid);
 }
