169305423edb9cd988de152ead8c5af482560b4d
LIM SEEUN <limse0204@naver.com>
2023-05-15 23:47:48 +0900

    [back] feat : 알림 서비스 추가

--- backend/notificationService/src/main/java/com/ssafy/notificationservice/service/NotificationService.java
+++ backend/notificationService/src/main/java/com/ssafy/notificationservice/service/NotificationService.java
@@ -1,4 +1,11 @@
 package com.ssafy.notificationservice.service;
 
+import com.ssafy.notificationservice.vo.NotificationVO;
+
 public interface NotificationService {
+    void createTopicOpenNotification(Long memberId);
+    void createTopicWinnerNotification(NotificationVO notificationVO);
+    void createMentionNotification(NotificationVO notificationVO);
+    void createTeamVoteNotification(NotificationVO notificationVO);
+
 }
--- backend/notificationService/src/main/java/com/ssafy/notificationservice/service/NotificationServiceImpl.java
+++ backend/notificationService/src/main/java/com/ssafy/notificationservice/service/NotificationServiceImpl.java
@@ -1,8 +1,43 @@
 package com.ssafy.notificationservice.service;
 
+import com.ssafy.notificationservice.jpa.Gender;
+import com.ssafy.notificationservice.jpa.NotificationEntity;
+import com.ssafy.notificationservice.jpa.NotificationRepository;
+import com.ssafy.notificationservice.jpa.Type;
+import com.ssafy.notificationservice.vo.NotificationVO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService{
+    private final NotificationRepository notificationRepository;
+    @Override
+    @Transactional
+    public void createTopicOpenNotification(Long memberId) {
+        NotificationEntity notification = NotificationEntity.builder()
+                .memberId(memberId)
+                .type(Type.TOPIC_OPEN)
+                .gender(Gender.UNSUPPORTED)
+                .title("토픽 응모가 시작되었습니다~☘️")
+                .build();
 
+        notificationRepository.save(notification);
+    }
+
+    @Override
+    public void createTopicWinnerNotification(NotificationVO notificationVO) {
+
+    }
+
+    @Override
+    public void createMentionNotification(NotificationVO notificationVO) {
+
+    }
+
+    @Override
+    public void createTeamVoteNotification(NotificationVO notificationVO) {
+
+    }
 }
