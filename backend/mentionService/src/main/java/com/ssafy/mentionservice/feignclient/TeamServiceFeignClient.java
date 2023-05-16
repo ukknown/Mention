31ebaf1b75f0865090aa596db9e54ccba75c03dd
조승현 <sansusjc3@naver.com>
2023-05-16 15:15:50 +0900

    [back] feat : 팀 구성원의 id 리스트 feign 작성

--- backend/mentionService/src/main/java/com/ssafy/mentionservice/feignclient/TeamServiceFeignClient.java
+++ backend/mentionService/src/main/java/com/ssafy/mentionservice/feignclient/TeamServiceFeignClient.java
@@ -4,8 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @FeignClient(name = "team-service")
 public interface TeamServiceFeignClient {
     @GetMapping("/team-service/teams/count/members/{teamid}")
     int getTeamMemberCount(@PathVariable Long teamid);
+    @GetMapping("/team-service/teams/members/{teamid}")
+    List<Long> getMemberIdList(@PathVariable Long teamid);
 }
