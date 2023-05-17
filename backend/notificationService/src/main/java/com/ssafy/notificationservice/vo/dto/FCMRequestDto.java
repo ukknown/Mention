c7629ed85035359d644b76ee067e44d713e91a34
조승현 <sansusjc3@naver.com>
2023-05-18 00:52:39 +0900

    [back] feat : routingId 추가

--- backend/notificationService/src/main/java/com/ssafy/notificationservice/vo/dto/FCMRequestDto.java
+++ backend/notificationService/src/main/java/com/ssafy/notificationservice/vo/dto/FCMRequestDto.java
@@ -1,6 +1,5 @@
 package com.ssafy.notificationservice.vo.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +10,12 @@ public class FCMRequestDto {
     private Long targetMemberId;
     private String title;
     private String body;
+    private Long routingId;
     @Builder
-    public FCMRequestDto(Long targetMemberId, String title, String body) {
+    public FCMRequestDto(Long targetMemberId, String title, String body, Long routingId) {
         this.targetMemberId = targetMemberId;
         this.title = title;
         this.body = body;
+        this.routingId = routingId;
     }
 }
