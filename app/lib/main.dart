9fcb3b625e6e2f5be42b2fafaeaec919bc0c6830
조승현 <sansusjc3@naver.com>
2023-05-19 02:48:49 +0900

    [front] feat : fcm alarm

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -11,16 +11,52 @@ import 'package:flutter/material.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'fcmSetting.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+// 앱이 종료된 상태에서 알림 클릭 시 이동을 위해 필요한 navigatorKey
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  String? firebaseToken = await fcmSetting();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   if (!prefs.containsKey('isRead')) {
     await prefs.setStringList('isRead', []);
   }
   KakaoSdk.init(nativeAppKey: 'fdb226da9bdf3e5030a432d9f509a01a');
   await AndroidAlarmManager.initialize();
+
   runApp(new Mention());
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
+
+  NotificationAppLaunchDetails? details =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  if (details != null && details.didNotificationLaunchApp) {
+    selectNotification('이동해라 얍');
+  }
+}
+
+Future selectNotification(String? payload) async {
+  // Handle notification tap
+  if (payload != null && payload.isNotEmpty) {
+    navigatorKey.currentState!
+        .push(MaterialPageRoute(builder: (context) => NoticePage()));
+  }
 }
 
 class Mention extends StatelessWidget {
@@ -41,6 +77,7 @@ class Mention extends StatelessWidget {
         ),
         cardColor: const Color(0xFFF4EDDB),
       ),
+      navigatorKey: navigatorKey, // Add navigatorKey
       home: const MainPage(),
     );
   }
